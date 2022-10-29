1bfeeb3aa5cbb339d920ac8a5467afd8ab0e34ca
Musab1258 <habeebmusab@gmail.com>
2022-10-29 15:19:44 +0000

    Created the jsx of the profile section

--- musab-linktree/src/components/profile/Profile.js
+++ musab-linktree/src/components/profile/Profile.js
@@ -1,8 +1,17 @@
 import './Profile.css';
+import musab from '../../images/musab.jpg';
 
 function Profile() {
     return (
         <div>
+            <img src={musab} alt='My profile pic' id='profile__img' className="profile-img" />
+            <p>Musab Habeeb</p>
+            <div className="btn"
+                 id='twitter'
+            >
+                <h4>Twitter Username</h4>
+                <h6>@MusabHabeeb2</h6>
+            </div>
 
         </div>
     )

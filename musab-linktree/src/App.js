fa428fee60749bcd64f0204036c0731053d713cb
Musab1258 <habeebmusab@gmail.com>
2022-10-29 15:31:25 +0000

    Updated the App component and styled it.

--- musab-linktree/src/App.js
+++ musab-linktree/src/App.js
@@ -1,12 +1,14 @@
-import Profile from './components/profile/Profile';
 import './App.css';
+import Profile from './components/profile/Profile';
 import Links from './components/links/Links';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Profile />
       <Links />
+      <Footer />
     </div>
   );
 }

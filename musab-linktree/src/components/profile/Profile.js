924225d1c831b601f2fb728c712b8ecb9234b230
Musab1258 <habeebmusab@gmail.com>
2022-10-29 15:22:53 +0000

    Styled the profile section.

--- musab-linktree/src/components/profile/Profile.js
+++ musab-linktree/src/components/profile/Profile.js
@@ -1,18 +1,20 @@
 import './Profile.css';
 import musab from '../../images/musab.jpg';
+import share from '../../images/share.svg';
+import dot from '../../images/dot.svg'
 
 function Profile() {
     return (
-        <div>
-            <img src={musab} alt='My profile pic' id='profile__img' className="profile-img" />
-            <p>Musab Habeeb</p>
-            <div className="btn"
-                 id='twitter'
-            >
-                <h4>Twitter Username</h4>
-                <h6>@MusabHabeeb2</h6>
+        <div className="profile">
+            <div className="profile-imgs">
+                <img src={musab} alt='My profile pic' id='profile__img' className="profile-img" />
+                <img src={dot} alt='dot button' className="profile-btn mobile" />
+                <img src={share} alt='share button' className="profile-btn desktop" />
             </div>
-
+            <div>
+                <p id='twitter'>@MusabHabeeb2</p>
+                <p id='slack' className="hidden">Musab1258</p>
+            </div>
         </div>
     )
 }
